b67cb9277a10421aba3d49fa56f85dc31b7569a1
Tyler Lynch <LynchTJ20@pwcs-edu.org>
2019-05-09 12:22:37 -0400

    Fixed name reversal

--- Task 2 - Person B/src/Project.java
+++ Task 2 - Person B/src/Project.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class Project
 {
-    public static void main()
+    public static void main(String args[])
     {
         System.out.println("What is your name?");
-        scan sc = new Scan(System.in);
-        String name = sc.next();
+        //Fixed Scanner class name
+        Scanner sc = new Scanner(System.in);
+        //Changed to sc.nextLine
+        String name = sc.nextLine();
         System.out.println("Your name backwards is:");
         String ans = "";
-        for (int i = name.length() - 1; i >= 0; i++)
+        // Fixed i++ to i--
+        for (int i = name.length() - 1; i >= 0; i--)
         {
             ans += name.charAt(i);
         }
