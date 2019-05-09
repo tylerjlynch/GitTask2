52c48f1edafd28715ec14a0e86de61dfbfed5ab9
Trevor Lew <LewTL19@pwcs-edu.org>
2019-05-09 12:05:26 -0400

    Person A

---
+++ GitTask2-A/src/Average.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Average {
+
+    public static void main (String [] args ) {
+        Scanner scan = new Scanner( System.in );
+        System.out.println( "Enter your first number: ");
+        int a = scan.nextInt();
+        System.out.println( "Enter your second number: ");
+        int b = scan.nextInt();
+        int average = (b - b)/2;
+        System.out.println( average );
+    }
+
+}
